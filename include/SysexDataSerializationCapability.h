a2cdd0bac8e03adea2ee11a848be9fb3488968f2
cruc <christof.ruch@gmx.de>
2019-12-01 20:36:12 +0100

    Adding copyright header and namespace.

--- include/SysexDataSerializationCapability.h
+++ include/SysexDataSerializationCapability.h
@@ -1,10 +1,19 @@
+/*
+   Copyright (c) 2019 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
 #pragma once
 
 #include "JuceHeader.h"
 
-class SysexDataSerializationCapability {
-public:
-	virtual bool dataFromSysex(const std::vector<MidiMessage> &message) = 0;
-	virtual std::vector<MidiMessage> dataToSysex() const = 0;
-};
+namespace midikraft {
 
+	class SysexDataSerializationCapability {
+	public:
+		virtual bool dataFromSysex(const std::vector<MidiMessage> &message) = 0;
+		virtual std::vector<MidiMessage> dataToSysex() const = 0;
+	};
+
+}
