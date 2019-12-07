5010781747d967307bd1a090c9eccd20c11c3364
cruc <christof.ruch@gmx.de>
2019-12-07 15:31:47 +0100

    Add more stuff into the SynthParameterDefinition - the Rev2 has vector-valued parameters. This class seems to get bloated now, so I will split it into multiple capabilities now.

--- include/SynthParameterDefinition.h
+++ include/SynthParameterDefinition.h
@@ -19,11 +19,23 @@ namespace midikraft {
 
 	class SynthParameterDefinition {
 	public:
-		virtual std::string name() const = 0;
+		//TODO - should I use inheritance? Or a capability interface again?
+		enum class ParamType {
+			INT,
+			INT_ARRAY
+		};
+
+		// Synth parameters are usually integers, or arrays of integers (e.g. gated sequencer)
+		virtual ParamType type() const = 0;
+
+		virtual std::string name() const = 0; // Short name
+		virtual std::string description() const = 0; // Long name / descriptive name
+
 		virtual std::string valueAsText(int value) const = 0;
+
 		virtual int sysexIndex() const = 0;
 		virtual int endSysexIndex() const = 0;
-		virtual std::string description() const = 0;
+
 		virtual bool matchesController(int controllerNumber) const = 0;
 		virtual int minValue() const = 0;
 		virtual int maxValue() const = 0;
@@ -31,7 +43,13 @@ namespace midikraft {
 		//! A parameter is deemed active when its value can have any meaningful effect on the sound produced
 		virtual bool isActive(Patch const *patch) const = 0;
 
+		//! Use this function to retrieve the integer value in the patch
 		virtual bool valueInPatch(Patch const &patch, int &outValue) const = 0;
+		//! Use this function to retrieve the integer array value in the patch
+		virtual bool valueInPatch(Patch const &patch, std::vector<int> &outValue) const = 0;
+
+		virtual void setInPatch(Patch &patch, int value) const = 0;
+		virtual void setInPatch(Patch &patch, std::vector<int> value) const = 0;
 
 		// This is for live editing!
 		virtual MidiBuffer setValueMessage(Patch const &patch, Synth *synth) const = 0;
