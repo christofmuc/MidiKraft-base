d94a38d49625c460128419398319515f11ebe469
cruc <christof.ruch@gmx.de>
2020-03-08 17:21:42 +0100

    Need more info per data type

--- include/DataFileLoadCapability.h
+++ include/DataFileLoadCapability.h
@@ -7,11 +7,17 @@ namespace midikraft {
 
 	class DataFileLoadCapability {
 	public:
+		struct DataFileDescription {
+			std::string name; // Display name of the type
+			bool canBeRequested; // If you can't request it, it is a write-only type. For example, the Alternate Tunings of the Rev2
+			bool canBeSent;  // If you can't send the file type, it is read-only. For example, the global settings of the Rev2
+		};
+
 		virtual std::vector<MidiMessage> requestDataItem(int itemNo, int dataTypeID) = 0;
 		virtual int numberOfDataItemsPerType(int dataTypeID) = 0;
 		virtual bool isDataFile(const MidiMessage &message, int dataTypeID) = 0;
 		virtual void loadData(std::vector<MidiMessage> messages, int dataTypeID) = 0;
-		virtual std::vector<std::string> dataTypeNames() = 0;
+		virtual std::vector<DataFileDescription> dataTypeNames() = 0;
 	};
 
 
