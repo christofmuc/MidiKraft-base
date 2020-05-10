6d4b0b12290bd0b4ebfcb38c048b3c83902f97e6
cruc <christof.ruch@gmx.de>
2020-05-10 14:30:15 +0200

    Create a new Keyboard capability, that can be used to describe the keyboard of a synth, and also whether it does actually have that keyboard (or is the module version)

--- include/MasterkeyboardCapability.h
+++ include/MasterkeyboardCapability.h
@@ -8,10 +8,21 @@
 
 #include "MidiChannel.h"
 #include "MidiController.h"
+#include "MidiNote.h"
 
 #include <functional>
 
 namespace midikraft {
+
+	class KeyboardCapability {
+	public:
+		//! This is used for synths that come in keyboard and desktop variety - should be configurable via the UI
+		virtual bool hasKeyboard() const = 0;
+		//! Returns the lowest physical key playable on the keyboard, without keyboard-local transpose
+		virtual MidiNote getLowestKey() const = 0;
+		//! Returns the highest physical key playable on the keyboard, without keyboard-local transpose
+		virtual MidiNote getHighestKey() const = 0;
+	};
 
 	class MasterkeyboardCapability {
 	public:
